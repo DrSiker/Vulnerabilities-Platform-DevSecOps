bcb53c17df50a7240533eeb7b8dd71388f6df2ac
DrSiker <94201937+DrSiker@users.noreply.github.com>
2025-02-21 17:47:17 -0500

    edit from config_app

--- Backend/app.py
+++ Backend/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from database import db, Vulnerability
-from backend.config_app import Config
+from config_app import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
