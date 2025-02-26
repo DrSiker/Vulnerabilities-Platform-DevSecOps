e314b1b3a3a02028b3acfd3f701fe0f16aea55d3
DrSiker <94201937+DrSiker@users.noreply.github.com>
2025-02-26 13:14:32 -0500

    add table name

--- Backend/database.py
+++ Backend/database.py
@@ -3,6 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class Vulnerability(db.Model):
+    
+    __tablename__ = "vulnerabilities"
+    
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.Text, nullable=False)
     severity = db.Column(db.String(50), nullable=False)
