a66928a75c40492a9bef14c914eb0d32b2160039
DrSiker <94201937+DrSiker@users.noreply.github.com>
2025-02-26 12:57:46 -0500

    edit init sql

--- db/init.sql
+++ db/init.sql
@@ -2,9 +2,9 @@ CREATE TABLE vulnerabilities (
     id SERIAL PRIMARY KEY,
     description TEXT NOT NULL,
     severity VARCHAR(50) NOT NULL,
-    tool VARCHAR(50) NOT NULL,
+    tool VARCHAR(50),
     file_path TEXT,
-    line_number INT,
+    line_number INTEGER,
     date_found TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    status VARCHAR(20) DEFAULT 'pending'
+    status VARCHAR(50) DEFAULT 'open'
 );
